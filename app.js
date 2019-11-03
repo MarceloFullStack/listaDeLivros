7275ba777e903e80f43d4ad3b590b4b0b947f40e
marcelo <fotofacil@live.com>
2019-11-02 22:18:09 -0300

    book class Feito, UI class feito, display feito. ainda estatico

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 // Book Class: Represents a Book
-
+class Book{
+    constructor(titulo, autor, isbn){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+}
 // UI Class: Handle UI Tasks
+class UI{
+    static displayBooks(){
+        const StoredBooks =[{
+            titulo:'Livro Um',
+            autor:'Marcelo Guimaraes',
+            isbn:'666666'
+        },
+    {
+        titulo:'Livro Sem paginas',
+        autor:'Smith',
+        isbn:'555555' 
+    }];
+    const books = StoredBooks;
 
+    books.forEach((book)=> UI.addBookToList(book));
+    }
+
+    static addBookToList(book){
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.titulo}</td> 
+        <td>${book.autor}</td> 
+        <td>${book.isbn}</td> 
+        <td><a href='#' class="btn btn-danger btn-sm delete">X</a></td> 
+        `;
+
+        list.appendChild(row)
+    }
+}
 // Store Class: Handles Storage
 
 // Event: Display Book
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Event: Add a Book
 
